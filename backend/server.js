0d3a978715a775403b3551d8daeb712daf693138
tk1475 <ahmedabclr35@gmail.com>
2024-07-30 13:11:46 +0500

    fixxx

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const ProfileRoutes = require('./routes/profileRoutes');
 const listingRoutes = require('./routes/sellerRoutes');
 const threadRoutes = require('./routes/threadRoutes'); 
 const listRoute = require('./routes/listingRoutes') 
+const communityRoute = require('./routes/communityRoutes')
  
 dotenv.config();
 
@@ -36,7 +37,9 @@ app.use('/api', UserRoute);
 app.use('/api', LoginRoute);
 app.use('/api', ProfileRoutes);
 app.use('/api/listings', listingRoutes);
-app.use('/api/threads', threadRoutes); app.use('/api/list/', listRoute)
+app.use('/api/threads', threadRoutes); 
+app.use('/api/list/', listRoute)
+app.use('/api/community', communityRoute)
 
 
 app.listen(port, () => {

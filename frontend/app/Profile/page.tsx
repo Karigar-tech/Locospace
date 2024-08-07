89b746e02aa10e4324d63612c77697021c475f85
tk1475 <ahmedabclr35@gmail.com>
2024-08-07 10:11:18 +0500

    hello

--- frontend/app/Profile/page.tsx
+++ frontend/app/Profile/page.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import React, { Suspense,useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../../components/NavBar";
 import UserProfile from "../../components/Profile/UserProfile";
 import Footer from "../../components/LandingFooter";
 import CardGridComp from "../../components/Profile/ListingCard";
 import "../../styles/profile.css";
 import { Listing } from "@/types";
-import { Thread } from '@/types';
-import ThreadBox  from '../../components/Threads/ThreadBox';
+import { Thread } from "@/types";
+import ThreadBox from "../../components/Threads/ThreadBox";
 
 const MyProfile: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState<"listings" | "threads">(
-    "listings"
-  );
+  const [selectedTab, setSelectedTab] = useState<"listings" | "threads">("listings");
   const [listings, setListings] = useState<Listing[]>([]);
   const [threads, setThreads] = useState<Thread[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+
   const handleProfileUpdate = (profile: { user: any; listings: Listing[] }) => {
     setListings(profile.listings);
   };
@@ -25,15 +24,14 @@ const MyProfile: React.FC = () => {
     setIsLoading(true);
     const getThreads = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/threads/userThreads', 
-          {method: "GET",
+        const response = await fetch('http://localhost:5000/api/threads/userThreads', {
+          method: "GET",
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${localStorage.getItem('token')}`,
           },
         });
         const data = await response.json();
-        console.log("ITS: ", data)
         setThreads(data);
       } catch (error) {
         console.log('Error fetching threads:', error);
@@ -50,49 +48,41 @@ const MyProfile: React.FC = () => {
       <UserProfile onProfileUpdate={handleProfileUpdate} />
       <div className="profile-toggle-container">
         <button
-          className={`profile-toggle-button ${
-            selectedTab === "listings" ? "active" : ""
-          }`}
+          className={`profile-toggle-button ${selectedTab === "listings" ? "active" : ""}`}
           onClick={() => setSelectedTab("listings")}
         >
           Listings
         </button>
         <button
-          className={`profile-toggle-button ${
-            selectedTab === "threads" ? "active" : ""
-          }`}
+          className={`profile-toggle-button ${selectedTab === "threads" ? "active" : ""}`}
           onClick={() => setSelectedTab("threads")}
         >
           Threads
         </button>
       </div>
-      <div className="content-container">
+      <div className="profile-content-container">
         {selectedTab === "listings" ? (
-          <div className="listings-content">
             <CardGridComp data={listings} />
-          </div>
         ) : (
-          <div className="threads-content">
-
-            {isLoading  ? (
-              <p>Loading..</p>
-            ) : threads!==null ? (
-              threads.map(thread =>(
-                <ThreadBox 
-                key={thread._id}
-                _id={thread._id}
-                user_id={thread.user_id}
-                community_id={thread.community_id}
-                thread_description={thread.thread_description}
-                thread_title={thread.thread_title}
-                createdAt={thread.createdAt}
-                updatedAt={thread.updatedAt}
+          <div className="profile-threads-content">
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : threads.length > 0 ? (
+              threads.map((thread) => (
+                <ThreadBox
+                  key={thread._id}
+                  _id={thread._id}
+                  user_id={thread.user_id}
+                  community_id={thread.community_id}
+                  thread_description={thread.thread_description}
+                  thread_title={thread.thread_title}
+                  createdAt={thread.createdAt}
+                  updatedAt={thread.updatedAt}
                 />
               ))
             ) : (
-              <p>Alaka Threads Nishta</p>
+              <p>No threads available</p>
             )}
-            
           </div>
         )}
       </div>
